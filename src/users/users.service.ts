ade44205a1b184d1a2e7fa3aab3bc702e1badc5a
Lameck Santos <lameck2@hotmail.com>
2025-03-22 15:46:31 -0300

    feat(-resources): Add users and posts resources

---
+++ src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  create(createUserDto: CreateUserDto) {
+    Logger.log(createUserDto);
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    Logger.log(updateUserDto);
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
